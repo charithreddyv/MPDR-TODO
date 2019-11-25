9525335b4b8d168bfca0b7d207537e7d04a3e417
charithreddyv <charithreddy889@gmail.com>
2019-11-25 23:34:06 +0530

    updated CURD :pencil: operations :heavy_check_mark:

--- frontended/src/App.js
+++ frontended/src/App.js
@@ -1,11 +1,13 @@
 import React,{ useEffect, useState } from 'react';
 import axios from 'axios';
-import { Button, CardColumns } from 'react-bootstrap';
+import { Button, CardColumns, Modal, Form } from 'react-bootstrap';
 import TodoItem from './components/todoitems';
 
 
 function App() {
   const [todolist, settodo] = useState([]);
+  const [itemShow, setItemShow] = useState(false);
+  const [todoItem, setTodoItem ] = useState({title: '', description: '', completed: false})
   
   useEffect( () => {
     axios.get('todos')
@@ -17,6 +19,7 @@ function App() {
       })
 
   },[])
+
   const deleteitem = (id) => {
     axios.delete('todo/'+ id)
     .then(res => {
@@ -24,18 +27,61 @@ function App() {
     })
     .catch(err => console.log(err))
   }
+
+  const createTodoItem = () => {
+    axios.post('todos/', {"todo": todoItem})
+    .then(res => {
+      setItemShow(false)
+    })
+    .catch(err => setItemShow(false))
+  }
+
+  const resetEdited = (data) => {
+    settodo(
+      todolist.map(todo => {
+        if (todo.id === data.id) {
+          return {...todo, ...data.todo}
+        }
+        else {
+          return todo
+        }
+      })
+    )
+  }
+
   return (
     <div>
-      <Button style={{marginLeft: '50%', marginTop: '10px', marginBottom: '10px'}}>Add TODO Item</Button>
+      <Button  onClick={() => setItemShow(true)} style={{marginLeft: '50%', marginTop: '10px', marginBottom: '10px'}}>Add TODO Item</Button>
       <br />
         <CardColumns style={{marginLeft: '12px', marginRight: '12px'}}>
         {
           todolist.map(data =>{
-            return <TodoItem  key={data.id} data={data}  deleteitem={(id) => deleteitem(id)}/>
+            return <TodoItem  key={data.id} data={data}  deleteitem={(id) => deleteitem(id)} editTodo={(data) => resetEdited(data) }/>
           } 
             )
         }
         </CardColumns>
+        <Modal show={itemShow}>
+          <Modal.Header>
+            <Modal.Title>create Todo</Modal.Title>
+          </Modal.Header>
+          <Modal.Body>
+            <Form>
+              <Form.Group>
+                <Form.Label> Title: </Form.Label>
+                <Form.Control type="text" placeholder="Enter Title" value={todoItem.title || ''} onChange={(e) => setTodoItem({...todoItem, title: e.target.value})}/>
+              </Form.Group>
+              <Form.Group>
+                <Form.Label>Description: </Form.Label>
+                <Form.Control type="text" placeholder="Description" value={todoItem.description || ''} onChange={(e) => setTodoItem({...todoItem, description: e.target.value})}/>
+              </Form.Group>
+            </Form>
+          </Modal.Body>
+          <Modal.Footer>
+            <Button variant='outline-danger' onClick={() => setItemShow(false)}>Close</Button>
+            <Button variant='outline-primary' onClick={createTodoItem}> Save </Button>
+          </Modal.Footer>
+        </Modal>
     </div>
   );
 }
--- frontended/src/components/todoitems.js
+++ frontended/src/components/todoitems.js
@@ -1,23 +1,63 @@
-import React from 'react'
-import { Card, Button, Row} from 'react-bootstrap';
+import React, { useState } from 'react';
+import { Card, Button, Row, Modal, Form} from 'react-bootstrap';
+import axios from 'axios';
 
-const todoItem = (props) => {
+const TodoItem = (props) => {
+  const [showEdit, setShowEdit] = useState(false);
+  const [todoItem, setTodoItem ] = useState({title: '', description: '', completed: ''})
+
+  const submitEdit = (id) => {
+   axios.put('todo/'+id+'/', {"todo": todoItem})
+   .then(res => {
+     setShowEdit(false)
+     props.editTodo({todo: todoItem, id})
+   })
+   .catch(err => {
+     console.log(err)
+   })
+  }
   return (
-        <Card bg="white" text='grey' border="primary"  style={{width: '28rem', paddingLeft: '10px'}}>
-                <Card.Body>
-                  <Card.Title>
-                    {props.data.title}
-                  </Card.Title>
-                <Card.Text style={{textAlign: 'center'}}>{props.data.description}</Card.Text>
-                </Card.Body>
-                <Card.Footer>
-                  <Row>
-                    <Button variant="outline-primary" size="sm" style={{marginRight: "10px"}}>Edit</Button>
-                    <Button variant="outline-danger"  size="sm" onClick={() => props.deleteitem(props.data.id)} style={{marginLeft: 'auto'}}> Delete</Button>
-                  </Row>
-                </Card.Footer>
-              </Card>
+    <div>
+    <Card bg="white" text='grey' border="primary"  style={{width: '28rem', paddingLeft: '10px'}}>
+      <Card.Body>
+        <Card.Title>
+          {props.data.title}
+        </Card.Title>
+      <Card.Text style={{textAlign: 'center'}}>{props.data.description}</Card.Text>
+      </Card.Body>
+      <Card.Footer>
+        <Row>
+          <Button variant="outline-primary" size="sm" style={{marginRight: "10px"}} onClick={() => {
+            setTodoItem({title: props.data.title, description: props.data.description, completed: props.data.completed})
+            setShowEdit(true)
+          }}>Edit</Button>
+          <Button variant="outline-danger"  size="sm" onClick={() => props.deleteitem(props.data.id)} style={{marginLeft: 'auto'}}> Delete</Button>
+        </Row>
+      </Card.Footer>
+    </Card>
+    <Modal show={showEdit}>
+      <Modal.Header>
+        <Modal.Title>Edit Todo</Modal.Title>
+      </Modal.Header>
+      <Modal.Body>
+        <Form>
+          <Form.Group>
+            <Form.Label> Title: </Form.Label>
+            <Form.Control type="text" placeholder="Enter Title" value={todoItem.title || ''} onChange={(e)=> setTodoItem({...todoItem, title: e.target.value }) }/>
+          </Form.Group>
+          <Form.Group>
+            <Form.Label>Description: </Form.Label>
+            <Form.Control type="text" placeholder="Description"  value={todoItem.description || ''} onChange={(e)=> setTodoItem({...todoItem, description: e.target.value }) }/>
+          </Form.Group>
+        </Form>
+      </Modal.Body>
+      <Modal.Footer>
+        <Button variant='outline-danger' onClick={() => setShowEdit(false)}>Close</Button>
+        <Button variant='outline-primary' onClick={() => submitEdit(props.data.id)}> Save </Button>
+      </Modal.Footer>
+    </Modal>
+  </div> 
   );
 }
 
-export default todoItem;
+export default TodoItem;
