0991aba7b72bf5d84c3400ecb9fadb98c9ad65e9
charithreddyv <charithreddy889@gmail.com>
2019-11-26 11:16:41 +0530

    Typo fixtures :innocent:

--- apiv1/serializers.py
+++ apiv1/serializers.py
@@ -4,7 +4,7 @@ from rest_framework import serializers
 class TodoSerializer(serializers.Serializer):
     id = serializers.ReadOnlyField()
     title = serializers.CharField(max_length=20)
-    description = serializers.CharField(max_length=100)
+    description = serializers.CharField(max_length=100, allow_null=True)
     completed = serializers.BooleanField()
 
     def create(self, validated_data):
@@ -13,7 +13,7 @@ class TodoSerializer(serializers.Serializer):
     
     def update(self, instance, validated_data):
         instance.title = validated_data.get('title', instance.title)
-        instance.description = validated_data.get('description', instance.description, allow_null=True)
+        instance.description = validated_data.get('description', instance.description)
         instance.completed = validated_data.get('completed', instance.completed)        
         instance.save()
         return instance
